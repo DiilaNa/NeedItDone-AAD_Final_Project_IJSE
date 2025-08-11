4172675a19c161e6618919036a91436c14ebb601
DiilaNa <liyanaarachchidilan@gmail.com>
2025-08-11 18:42:01 +0530

    Add Js for Side Navigation Bar in AdminDashBoard

---
+++ FrontEnd/Js/AdminDashBoard.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+    sideNavBar();
+
+});
+
+function sideNavBar() {
+    var $circleMenuBtn = $('#circleMenuBtn');
+    var $hamburgerIcon = $('#hamburgerIcon');
+    var $sideNav = $('#sideNav');
+    var $navBackdrop = $('#navBackdrop');
+
+
+    $circleMenuBtn.on('click', function() {
+        if ($sideNav.hasClass('show')) {
+            $sideNav.removeClass('show');
+            $navBackdrop.removeClass('show');
+            $hamburgerIcon.removeClass('active');
+            $circleMenuBtn.removeClass('active');
+        } else {
+            $sideNav.addClass('show');
+            $navBackdrop.addClass('show');
+            $hamburgerIcon.addClass('active');
+            $circleMenuBtn.addClass('active');
+        }
+    });
+
+    $navBackdrop.on('click', function() {
+        $sideNav.removeClass('show');
+        $navBackdrop.removeClass('show');
+        $hamburgerIcon.removeClass('active');
+        $circleMenuBtn.removeClass('active');
+    });
+
+    $('.nav-menu a').on('click', function() {
+        if ($(window).width() <= 991) {
+            $sideNav.removeClass('show');
+            $navBackdrop.removeClass('show');
+            $hamburgerIcon.removeClass('active');
+            $circleMenuBtn.removeClass('active');
+        }
+    });
+
+}
