9a5af5e0c8c801c0c2ef7199a351e98cad218c04
DiilaNa <liyanaarachchidilan@gmail.com>
2025-08-15 19:46:07 +0530

    Add UserRepository

---
+++ BackEnd/src/main/java/lk/ijse/project/backend/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package lk.ijse.project.backend.repository;
+
+import lk.ijse.project.backend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+}
