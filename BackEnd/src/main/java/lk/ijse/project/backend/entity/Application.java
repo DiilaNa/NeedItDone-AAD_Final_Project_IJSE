2047ea70af274b2c83bf888dde51c54f1fed9f8c
DiilaNa <liyanaarachchidilan@gmail.com>
2025-08-18 19:00:59 +0530

    Add Entity Application

---
+++ BackEnd/src/main/java/lk/ijse/project/backend/entity/Application.java
@@ -0,0 +1,29 @@
+package lk.ijse.project.backend.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Application {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String jobTitle;
+    private String category;
+    private Date date;
+    private String status;
+    private Date amount;
+
+}
